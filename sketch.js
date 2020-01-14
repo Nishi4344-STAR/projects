d9bb3899c1ed5e1736913f9681a557ac10c7ab5f
veersaraf <44894339+veersaraf@users.noreply.github.com>
2020-01-14 16:09:28 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 var bird, birdImage, gameOver, gameOverImage, edges, replay, replayImage ;
 var PLAY = 1;
 var END = 0;
+var score= 0;
 var gameState = PLAY;
 var pillar1, pillar2, pillarGroup1, pillarGroup2;
 function preload()
@@ -18,9 +19,9 @@ function setup()
   bird.addImage(birdImage);
   bird.scale = 0.15;
 
-  gameOver = createSprite(250, 200, 50, 50);
+  gameOver = createSprite(250, 400, 50, 50);
   gameOver.addImage(gameOverImage);
- // gameOver.visible = false;
+  gameOver.visible = false;
   gameOver.scale = 0.5;
 
   edges = createEdgeSprites();
@@ -28,9 +29,9 @@ function setup()
   pillarGroup1 = new Group();
   pillarGroup2 = new Group();
   
-  replay = createSprite(250, 300, 50, 50);
+  replay = createSprite(250, 600, 50, 50);
   replay.addImage(replayImage);
-  //replay.visible = false;
+   replay.visible = false;
   replay.scale = 0.25;
 }
 
@@ -39,12 +40,12 @@ function draw()
   background(0); 
   if(gameState===PLAY)
   {
-    if(keyDown("space"))
+    if(keyWentDown("space"))
     {
       bird.velocityY = -8;
     }
 
-    if (/*bird.isTouching(edges[2]) || bird.isTouching(edges[3]) ||*/ pillarGroup1.isTouching(bird) || pillarGroup2.isTouching(bird)) 
+    if (bird.isTouching(edges[2]) || bird.isTouching(edges[3]) || pillarGroup1.isTouching(bird) || pillarGroup2.isTouching(bird)) 
     {
       gameState = END;
       console.log(gameState);
@@ -55,30 +56,31 @@ function draw()
     createPillar1();
     createPillar2();
 
-    drawSprites();
   }
   else if(gameState===END)
   {
-    console.log("ITS GETTING Clled");
     endGame();
   }
+  drawSprites();
+  text("score : " + score, 300, 100);
 }
 
 function createPillar1()
 {
   if(frameCount%60 === 0 && gameState===PLAY)
   {
-    pillar1 = createSprite(600, random(50, 150), 100, 400);
+    pillar1 = createSprite(600, random(50, 140), 100, 400);
     pillarGroup1.add(pillar1);
     pillarGroup1.lifetime = 400;
     pillarGroup1.setVelocityXEach(-5);
+
   }
 }
 function createPillar2()
 {
   if(frameCount%60 === 0 && gameState===PLAY)
   {
-    pillar2 = createSprite(600, random(625, 750), 100, 400);
+    pillar2 = createSprite(600, random(625, 760), 100, 400);
     pillarGroup2.add(pillar2);
     pillarGroup2.lifetime = 400;
     pillarGroup2.setVelocityXEach(-5);
@@ -92,5 +94,18 @@ function endGame()
   pillarGroup1.destroyEach();
   pillarGroup2.destroyEach();
   console.log("ENGAME IS ON");
+  if(mousePressedOver(replay))
+  {
+    restart();
+  }
   
+}
+function restart()
+{
+  replay.visible =false;
+  gameOver.visible = false;
+  bird.visible = true;
+  bird.y = 200;
+  score = 0;
+  gameState = PLAY;
 }
